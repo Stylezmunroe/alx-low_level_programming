7b93663dd6da24de1d43e2c3372b2abc3e7990ad
Stylezmunroe <Oyunrantijoshua@gmail.com>
2023-07-28 04:45:33 -0700

    print

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,33 +1,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
+
 /**
-* add_node_end - adds a new node at the end of a linked list
-* @head: double pointer to the list_t list
-* @str: string to put in the new node
-*
-* Return: address of the new element, or NULL if it failed
-*/
+ * add_node_end - adds a new person to the end of the line.
+ * @head: double pointer to the list_t list.
+ * @str: the name of the new person (a string).
+ *
+ * Return: Address of the new person, or NULL if it failed.
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new;
-list_t *temp = *head;
-unsigned int len = 0;
-while (str[len])
-len++;
-new = malloc(sizeof(list_t));
-if (!new)
-return (NULL);
-new->str = strdup(str);
-new->len = len;
-new->next = NULL;
-if (*head == NULL)
-{
-*head = new;
-return (new);
-}
-while (temp->next)
-temp = temp->next;
-temp->next = new;
-return (new);
+	list_t *new_person;
+	list_t *temp = *head;
+	unsigned int len = 0;
+
+	while (str[len] != ' ' && str[len] != '\t' && str[len] != '\0')
+		len++;
+
+	new_person = malloc(sizeof(list_t));
+	if (!new_person)
+		return (NULL);
+
+	new_person->str = strdup(str);
+	new_person->len = len;
+	new_person->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_person;
+		return (new_person);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new_person;
+
+	return (new_person);
 }
