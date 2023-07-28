cb1c02cbd520a1102277d196b8b9cf2d8aa1972e
Stylezmunroe <Oyunrantijoshua@gmail.com>
2023-07-28 04:56:04 -0700

    print

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,38 +4,38 @@
 
 /**
  * add_node_end - adds a new person to the end of the line.
- * @head: double pointer to the list_t list.
+ * @h: double pointer to the list_t list.
  * @str: the name of the new person (a string).
  *
  * Return: Address of the new person, or NULL if it failed.
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node_end(list_t **h, const char *str)
 {
-	list_t *new_person;
-	list_t *temp = *head;
+	list_t *new;
+	list_t *temp = *h;
 	unsigned int len = 0;
 
 	while (str[len] != ' ' && str[len] != '\t' && str[len] != '\0')
 		len++;
 
-	new_person = malloc(sizeof(list_t));
-	if (!new_person)
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
 
-	new_person->str = strdup(str);
-	new_person->len = len;
-	new_person->next = NULL;
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
 
-	if (*head == NULL)
+	if (*h == NULL)
 	{
-		*head = new_person;
-		return (new_person);
+		*h = new;
+		return (new);
 	}
 
 	while (temp->next)
 		temp = temp->next;
 
-	temp->next = new_person;
+	temp->next = new;
 
-	return (new_person);
+	return (new);
 }
