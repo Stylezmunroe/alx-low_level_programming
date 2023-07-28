8e30e73dde88e06a8006b9a6429c617f66243176
Stylezmunroe <Oyunrantijoshua@gmail.com>
2023-07-28 05:36:22 -0700

    print

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+
+/* Function prototype with attribute to execute before main */
 void first(void) __attribute__ ((constructor));
+
 /**
-* first - prints a sentence before the main
-* function is executed
-*/
+ * first - Prints a message before the main function runs.
+ *
+ * Executed automatically before the main function starts.
+ * Prints the following message:
+ * "You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+ */
 void first(void)
 {
-printf("You're beat! and yet, you must allow,\n");
-printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
