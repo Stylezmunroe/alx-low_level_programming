a11d9c92c7d1c951d21ed0cc085e99c354ba1588
Stylezmunroe <Oyunrantijoshua@gmail.com>
2023-10-03 15:32:45 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
- */
+* _strdup - Duplicate a string into a new memory space.
+* @str: Pointer to the input string to duplicate.
+*
+* Return:0
+*/
 char *_strdup(char *str)
 {
-	char *aaa;
-	int i, r = 0;
+char *duplicate;
+int length = 0;
 
-	if (str == NULL)
-		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
+if (str == NULL)
+return (NULL);
 
-	aaa = malloc(sizeof(char) * (i + 1));
+while (str[length] != '\0')
+length++;
 
-	if (aaa == NULL)
-		return (NULL);
+duplicate = malloc(sizeof(char) * (length + 1));
 
-	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
+if (duplicate == NULL)
+return (NULL);
 
-	return (aaa);
+for (int i = 0; i <= length; i++)
+duplicate[i] = str[i];
+
+return (duplicate);
 }
 
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,6 +1,6 @@
-##C - malloc, free
+0x0B. C - malloc, free
 
-####TASKS
+[200~####TASKS
 
 ####[0. Float like a butterfly, sting like a bee](0-create_array.c)
 
